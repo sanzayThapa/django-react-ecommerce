a15604dd977ab04f34eb4f7d5493dd5a1b94d148
Sanzay Thapa <sanjaythapa9808@gmail.com>
2024-11-19 17:07:03 +0545

    updated django admin

--- backend/backend/userauths/admin.py
+++ backend/backend/userauths/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Profile)
+
+class User_admin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'phone']
+    list_editable = ['email', 'phone']
+    list_filter = ['full_name']
+    search_fields = ['full_name']
+class User_profile(admin.ModelAdmin):
+    list_display = ['full_name', 'gender', 'country']
+    list_editable = ['gender', 'country']
+
+
+
+admin.site.register(User, User_admin)
+admin.site.register(Profile, User_profile)
