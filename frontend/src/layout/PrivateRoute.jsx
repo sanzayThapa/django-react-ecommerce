b7e44ce0b0f2f4da6266384286d3a174a1b6d94c
Sanzay Thapa <sanjaythapa9808@gmail.com>
2024-12-03 12:35:38 +0545

    updated private Route

--- frontend/src/layout/PrivateRoute.jsx
+++ frontend/src/layout/PrivateRoute.jsx
@@ -1,14 +1,16 @@
-import {Navigate } from 'react-router-dom'
-import {useAuthStore} from '../store/auth'
-import { children } from 'react'
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { useAuthStore } from '../store/auth';
 
-const PrivateRoute =({children}) => {
+const PrivateRoute = ({ children }) => {
+    const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
 
-    const loggedIn = useAuthStore((state) => state.isLoggedIn)()
-    return loggedIn ? <>{children}</>: <Navigate to={'/login'} />
-}
+    return isLoggedIn ? children : <Navigate to="/login" />;
+};
 
+// Prop validation using PropTypes
+PrivateRoute.propTypes = {
+    children: PropTypes.node.isRequired, // Ensures children is passed and is a valid React node
+};
 
-export default PrivateRoute
-
-
+export default PrivateRoute;
