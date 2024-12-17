9321ad1a40c887d101ee4cafb81c0e72e8a11526
Sanzay Thapa <sanjaythapa9808@gmail.com>
2024-12-17 21:14:16 +0545

    updated hosts

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8+%30_ke)wam758haz$eh4cgaq7-1bai0r+uxh-x%b6zs6^iqp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
